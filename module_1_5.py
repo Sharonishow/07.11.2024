ba536769862bd86e2e21785379ca16ef7260a313
Sharonishow <81215237+Sharonishow@users.noreply.github.com>
2024-11-12 21:44:23 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 'a', 'b'
+# = 1, 2, 'a', 'b'
+#print(immutable_var_tuple_)
+#mutable_list = [1, 2, 'a', 'b']
+#print(mutable_list)
+print(immutable_var)
+mutable_list = [1, 2, 'a', 'b', 'Modified']
+print(mutable_list)
