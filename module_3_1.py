db8051b11203467b0eff802d2a0f1614147af57b
Sharonishow <81215237+Sharonishow@users.noreply.github.com>
2024-11-25 12:02:52 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+def count_calls():
+   
+    global calls
+    calls += 1
+
+def string_info(string):
+
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+def is_contains(string, list_to_search):
+
+    count_calls()
+    string_lower = string.lower()
+    for item in list_to_search:
+        if string_lower == item.lower():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(calls)
